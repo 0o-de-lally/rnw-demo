a2ed21fe1a6cfc1c77862879127f34b90f325932
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-05-03 16:31:06 -0400

    can use vitest and webdriverio to render components

---
+++ tests/demo.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from 'vitest-browser-react'
+import { expect, test } from 'vitest'
+import App from '../src/App'
+
+test('can open app', async () => {
+  const screen = render(<App />);
+
+  await expect.element(screen.getByText('Hello, World!')).toBeVisible()
+})
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chrome',
+      provider: 'webdriverio',
+      headless: true,
+    },
+  }
+})

8bc1b409f4d3f66b0174f787a880c5d872d8bbe3
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-05-06 20:10:31 -0400

    update sdk

--- src/components/LedgerInfo.tsx
+++ src/components/LedgerInfo.tsx
@@ -25,23 +25,23 @@ const LedgerInfo: React.FC = () => {
       // Get the client from the app config
       const cfg = getConfig();
 
-      //////// TO USE THE APTOS ßSDK
+      // //////// TO USE THE APTOS SDK
       // let settings: AptosSettings = {
       //   clientConfig: {
       //     WITH_CREDENTIALS: false,
-      //     // HEADERS: {
-      //     //   'Content-Type': 'application/json',
-      //     // },
+      //     HEADERS: {
+      //       'barking-mad': 'application/json',
+      //       // 'Accept': 'application/json',
+      //     },
       //   },
-      //   network: Network.TESTNET,
+      //   network: Network.MAINNET,
       //   fullnode: cfg.apiUrl,
       // };
       // let config = new Aptos (new AptosConfig(settings));
-      let config = cfg.client;
 
       // Fetch ledger info
       console.log('Fetching ledger info...');
-      const info = await config.getLedgerInfo();
+      const info = await cfg.client.getLedgerInfo();
       console.log('Ledger info received:', info);
       setLedgerInfo(info);
     } catch (err) {

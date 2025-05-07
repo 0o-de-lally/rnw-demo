3a20b4820b566d62236ad3791547bce0b292d9bc
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-05-07 15:36:30 -0400

    patch testnet

--- tests/ledgerInfo.spec.tsx
+++ tests/ledgerInfo.spec.tsx
@@ -5,5 +5,5 @@ import App from '../src/App'
 test('ledger info', async () => {
   const screen = render(<App />);
 
-  await expect.element(screen.getByText('"chain_id": 2')).toBeVisible()
+  await expect.element(screen.getByText('Chain ID: 2')).toBeVisible()
 })

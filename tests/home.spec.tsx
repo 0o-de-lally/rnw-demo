01fa34210d763d1edb8f31db86ed5e9984099f22
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-07-09 17:50:48 -0400

    add home test

---
+++ tests/home.spec.tsx
@@ -0,0 +1,15 @@
+
+import { render } from "vitest-browser-react";
+import { expect, test } from "vitest";
+import HomeScreen from "../src/screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+test("home renders", async () => {
+  const screen = render(
+    <NavigationContainer>
+      <HomeScreen />
+    </NavigationContainer>
+  );
+
+  await expect.element(screen.getByText("Explorer")).toBeVisible();
+});

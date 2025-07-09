32ade7b0b726af7348647458a58b7c5f164e22c5
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-07-09 18:26:06 -0400

    update vite build

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,8 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: "build",
+      commonjsOptions: { transformMixedEsModules: true }
+
     },
     base: "/rnw-demo",
     optimizeDeps: {

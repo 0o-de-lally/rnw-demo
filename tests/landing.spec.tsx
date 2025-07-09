437c1cd2693be63c08006970c12925bf13af2b3b
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-07-09 17:45:43 -0400

    patch test for explorer

--- tests/landing.spec.tsx
+++ tests/landing.spec.tsx
@@ -3,7 +3,11 @@ import { expect, test } from "vitest";
 import App from "../src/App";
 
 test("can open app", async () => {
+
+  // force history to start at root
+  window.history.pushState({}, "Test", "/");
   const screen = render(<App />);
 
   await expect.element(screen.getByText("Explorer")).toBeVisible();
+  // TODO: the page loads but then becomes blank
 });

ab53ffcc2139db239b2a4e543a4f8de1819ca2d6
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-07-10 15:59:44 -0400

    more debug

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ export default function App() {
         <div style={{ position: "fixed", top: 0, left: 0, background: "#eee", zIndex: 9999, fontSize: 12, padding: 4 }}>
           <div>BASE_PATH: <b>{basePath}</b></div>
           <div>window.location.pathname: <b>{window.location.pathname}</b></div>
+          <div>window.location.href: <b>{window.location.href}</b></div>
+          <div>USE_HASH_ROUTER: <b>{USE_HASH_ROUTER ? "true" : "false"}</b></div>
+
         </div>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
